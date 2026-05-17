7c58d788eb708dd00ed8a9d85220f0d2d3d17824
Kyuruki <74808999+Kyuruki@users.noreply.github.com>
2026-05-16 21:01:28 -0400

    forward file from vercel to modal

--- buildguard/api/analyze.js
+++ buildguard/api/analyze.js
@@ -3,13 +3,23 @@ import fs from "fs";
 
 export default async function handler(req, res) {
   const form = formidable();
-  form.parse(req, (err, fields, files) => {
+  form.parse(req, async (err, fields, files) => {
     if (err) {
       return res.status(500).json({ error: "Failed to parse file" });
     }
     const filepath = files.file[0].filepath;
-    console.log(filepath);
-
-    return res.status(200).json({ status: "ok", filepath: filepath });
+    const filebytes = fs.readFileSync(filepath);
+    const filename = files.file[0].originalFilename;
+    const modalform = new FormData();
+    modalform.append("bill", filebytes, filename);
+    const modalResponse = await fetch(
+      "https://kyuruki--billguard-analyze.modal.run",
+      {
+        method: "POST",
+        body: modalform,
+      },
+    );
+    const data = await modalResponse.json();
+    return res.status(200).json(data);
   });
 }
--- buildguard/backend.py
+++ buildguard/backend.py
@@ -1,5 +1,5 @@
 import modal
-
+from fastapi import FastAPI, File, UploadFile
 image = modal.Image.debian_slim().pip_install("fastapi[standard]")
 
 app = modal.App("billguard", image=image)
@@ -8,3 +8,8 @@ app = modal.App("billguard", image=image)
 @modal.fastapi_endpoint()
 def health():
     return {"status": "ok", "message": "BillGuard Modal alive"}
+
+@app.function()
+@modal.fastapi_endpoint(method="POST")
+async def analyze(bill: UploadFile = File(...)):
+    return {"status": "Ok", "filename": bill.filename}
