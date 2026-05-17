e299d9fa9bf363c591badacd73d644df6bff6aa0
Kyuruki <74808999+Kyuruki@users.noreply.github.com>
2026-05-16 20:37:51 -0400

    analyze handler for formidable

--- buildguard/api/analyze.js
+++ buildguard/api/analyze.js
@@ -1,7 +1,15 @@
+import formidable from "formidable";
+import fs from "fs";
+
 export default async function handler(req, res) {
-  const modalResponse = await fetch(
-    "https://kyuruki--billguard-health.modal.run",
-  );
-  const data = await modalResponse.json();
-  res.status(200).json(data);
+  const form = formidable();
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      return res.status(500).json({ error: "Failed to parse file" });
+    }
+    const filepath = files.file[0].filepath;
+    console.log(filepath);
+
+    return res.status(200).json({ status: "ok", filepath: filepath });
+  });
 }
